4d793e2ea75771ed1246382ce15493342b78b4e0
devxcdy <devxcdy@gmail.com>
2023-02-17 22:23:31 +0900

    first commit

---
+++ src/main/java/com/dev/pass/passbatchdev/PassBatchDevApplication.java
@@ -0,0 +1,13 @@
+package com.dev.pass.passbatchdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassBatchDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassBatchDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/pass/passbatchdev/PassBatchDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.pass.passbatchdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassBatchDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
